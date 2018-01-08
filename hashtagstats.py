0e738bdaca1c446bac6b5319f5779652229019c7
din <s.dina.e@gmail.com>
2018-01-08 05:37:17 -0500

    hashtagstats

--- hashtagstats.py
+++ hashtagstats.py
@@ -4,4 +4,9 @@ import sys
 from collections import defaultdict
 import json
 
+def get_hashtags(tweet):
+    entities = tweet.get('entities', {})
+    hashtags = entities.get('hashtags', [])
+    return [tag['text'].lower() for tag in hashtags]
+
 
