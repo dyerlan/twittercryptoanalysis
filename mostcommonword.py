095f0d429dba0462903c3266248cfd6623f9ec3f
din <s.dina.e@gmail.com>
2018-01-08 05:47:17 -0500

    hashtagstats

--- mostcommonword.py
+++ mostcommonword.py
@@ -1,3 +1,15 @@
 #Print the most common word
 #To Run: python moscommonword.py <file.jsonl>
 
+import sys
+import json
+from collections import Counter
+import re
+from nltk.corpus import stopwords
+import string
+
+
+punctuation = list(string.punctuation)
+stop = stopwords.words('english') + punctuation + ['rt', 'via']
+
+
